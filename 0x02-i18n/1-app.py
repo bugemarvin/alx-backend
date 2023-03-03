c47f0fa62ebdd7b0fe8002ef9da35549769b7913
Marvin Kurland <bugemarvin@outlook.com>
2023-03-03 18:52:17 +0300

    nstantiate the Babel object in app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -9,20 +9,21 @@ from flask_babel import Babel
 
 
 class Config:
-        '''
-        creating languages
-        'en' 'fr'
-        '''
-        BABEL_DEFAULT_TIMEZONE = 'UTC'
-        BABEL_DEFAULT_LOCALE = 'en'
-        LANGUAGES = ["en", "fr"]
+    '''
+    creating languages
+    'en' 'fr'
+    '''
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+    LANGUAGES = ["en", "fr"]
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
+
 @app.route('/')
 @app.route('/templates/1-index.html')
-
 def index() -> str:
-        return render_template('1-index.html')
+    return render_template('1-index.html')
