cc103489fe48ede950ce4c75bb1cbe022bb6f26d
Marvin Kurland <bugemarvin@outlook.com>
2023-03-04 00:13:33 +0300

    config langs

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-'''
-creating a localization
+'''creating a localization
 '''
 
 from flask_babel import Babel, _
@@ -8,8 +7,7 @@ from flask import render_template, Flask, request
 
 
 class Config:
-    '''
-    creating languages
+    '''creating languages cfg
     '''
     BABEL_DEFAULT_TIMEZONE = 'UTC'
     BABEL_DEFAULT_LOCALE = 'en'
@@ -23,8 +21,7 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    '''
-    determining the best language
+    '''determining the best language
     '''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
@@ -32,7 +29,6 @@ def get_locale():
 @app.route("/")
 @app.route("/templates/3-index.html")
 def index() -> str:
-    '''
-    view web page in accordance with locale
+    '''view web page in accordance with locale
     '''
     return render_template('3-index.html')
