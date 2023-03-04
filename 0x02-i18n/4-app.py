5d401b28e5271ad7dec4eca4932e75c585847d1a
Marvin Kurland <bugemarvin@outlook.com>
2023-03-04 11:45:36 +0300

    implementing request to take arguments languages

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""_defining languages_
+
+Returns:
+    _type_: flask application
+"""
+
+
+from flask_babel import Babel, _
+from flask import render_template, Flask, request
+
+
+class Config:
+    """_config file for flask_bable_
+    """
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """_Language Localisation_
+
+    Returns:
+        Str: prefered user language
+    """
+    try:
+        language = [i for i in Config.LANGUAGES]
+    except ValueError:
+        language = None
+    if language is not None:
+        for j in language:
+            locale = request.args.get('locale')
+            if locale == j:
+                return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+@app.route('/templates/4-index.html')
+def index() -> str:
+    """_Page Render_
+
+    Returns:
+        str: Content of html
+    """
+    return render_template('4-index.html')
