117c51d81dc0de2ffceefcdb47296501ed105acd
Marvin Kurland <bugemarvin@outlook.com>
2023-03-29 23:58:25 +0300

    creating hash sets

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,36 +1,43 @@
-#!/usr/bin/yarn dev
-import { createClient, print } from 'redis';
+import { promisify } from "util";
+import { createClient, print } from "redis";
 
 const client = createClient();
+async () => {
+  await client.connected();
+};
 
-client.on('error', (err) => {
-  console.log('Redis client not connected to the server:', err.toString());
+client.on("ready", () => {
+  console.log("Redis client connected to the server");
 });
 
-const updateHash = (hashName, fieldName, fieldValue) => {
-  client.HSET(hashName, fieldName, fieldValue, print);
-};
+client.on("Error", (err) => {
+  console.log("Redis client not connected to the server:", err);
+});
 
-const printHash = (hashName) => {
-  client.HGETALL(hashName, (_err, reply) => console.log(reply));
-};
-
-function main() {
-  const hashObj = {
-    Portland: 50,
-    Seattle: 80,
-    'New York': 20,
-    Bogota: 20,
-    Cali: 40,
-    Paris: 2,
-  };
-  for (const [field, value] of Object.entries(hashObj)) {
-    updateHash('HolbertonSchools', field, value);
-  }
-  printHash('HolbertonSchools');
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, response) => {
+    if (err) console.log(err);
+    if (response === "OK") {
+      console.log("Reply:", response);
+    }
+  });
 }
 
-client.on('connect', () => {
-  console.log('Redis client connected to the server');
-  main();
-});
+async function displaySchoolValue(schoolName) {
+  const clientAsync = promisify(client.get).bind(client);
+  try {
+    const response = await clientAsync(schoolName);
+    console.log(response);
+  } catch (err) {
+    const errors = await clientAsync(err);
+    console.log(errors);
+  }
+}
+
+client.HSET("HolbertonSchools", 'Portland', 50, print);
+client.HSET("HolbertonSchools", "Seattle", 80, print);
+client.HSET("HolbertonSchools", 'New York', 20, print);
+client.HSET("HolbertonSchools", 'Bogota', 20, print);
+client.HSET("HolbertonSchools", 'Cali', 40, print);
+client.HSET("HolbertonSchools", 'Paris', 2, print);
+client.hgetall('HolbertonSchools', (_error, value) => console.log(value));
