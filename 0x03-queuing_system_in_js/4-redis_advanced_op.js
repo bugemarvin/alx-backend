d366cb728c5876498f10f2066c0f09142c3cdb60
Marvin Kurland <bugemarvin@outlook.com>
2023-03-30 00:03:51 +0300

    hget && hgetall

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -14,30 +14,18 @@ client.on("Error", (err) => {
   console.log("Redis client not connected to the server:", err);
 });
 
-function setNewSchool(schoolName, value) {
-  client.set(schoolName, value, (err, response) => {
-    if (err) console.log(err);
-    if (response === "OK") {
-      console.log("Reply:", response);
-    }
-  });
-}
+client.hset("HolbertonSchools", "Portland", 50, print);
+client.hset("HolbertonSchools", "Seattle", 80, print);
+client.hset("HolbertonSchools", "New York", 20, print);
+client.hset("HolbertonSchools", "Bogota", 20, print);
+client.hset("HolbertonSchools", "Cali", 40, print);
+client.hset("HolbertonSchools", "Paris", 2, print);
 
-async function displaySchoolValue(schoolName) {
-  const clientAsync = promisify(client.get).bind(client);
-  try {
-    const response = await clientAsync(schoolName);
-    console.log(response);
-  } catch (err) {
-    const errors = await clientAsync(err);
-    console.log(errors);
-  }
-}
+const hgetallAsync = promisify(client.hgetall).bind(client);
 
-client.HSET("HolbertonSchools", 'Portland', 50, print);
-client.HSET("HolbertonSchools", "Seattle", 80, print);
-client.HSET("HolbertonSchools", 'New York', 20, print);
-client.HSET("HolbertonSchools", 'Bogota', 20, print);
-client.HSET("HolbertonSchools", 'Cali', 40, print);
-client.HSET("HolbertonSchools", 'Paris', 2, print);
-client.hgetall('HolbertonSchools', (_error, value) => console.log(value));
+const runAsync = async () => {
+  const result = await hgetallAsync("HolbertonSchools");
+  console.log(result);
+};
+
+runAsync();
