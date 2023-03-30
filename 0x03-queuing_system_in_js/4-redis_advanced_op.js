95bff25b0d4b6aa43eefc893f58934f7955d7501
Marvin Kurland <bugemarvin@outlook.com>
2023-03-30 13:00:03 +0300

    store a hash value using sets and hgetall

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -14,12 +14,24 @@ client.on("Error", (err) => {
   console.log("Redis client not connected to the server:", err);
 });
 
-client.hset("HolbertonSchools", "Portland", 50, print);
-client.hset("HolbertonSchools", "Seattle", 80, print);
-client.hset("HolbertonSchools", "New York", 20, print);
-client.hset("HolbertonSchools", "Bogota", 20, print);
-client.hset("HolbertonSchools", "Cali", 40, print);
-client.hset("HolbertonSchools", "Paris", 2, print);
+const getInfo = (key, field, value) => {
+  client.hset(key, field, value, () => {
+    print(null, 1);
+  });
+};
+
+const info = {
+  'Portland': "50",
+  'Seattle': "80",
+  'New York': "20",
+  'Bogota': "20",
+  'Cali': "40",
+  'Paris': "2",
+};
+
+for (const [key, values] of Object.entries(info)) {
+  getInfo("HolbertonSchools", key, values);
+}
 
 const hgetallAsync = promisify(client.hgetall).bind(client);
 
