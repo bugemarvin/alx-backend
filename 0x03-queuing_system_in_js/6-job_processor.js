9fe9ad23c90c75b82c5a73302cfed9a8484ca45c
Marvin Kurland <bugemarvin@outlook.com>
2023-04-24 11:34:08 +0300

    fixing es lint starndards

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,30 +1,33 @@
-import kue from 'kue';
-import  redis from 'redis';
-
+import kue from "kue";
+import redis from "redis";
 
 const redisClient = redis.createClient();
-const queue = kue.createQueue({redis: redisClient});
-const job = queue.create('push_notification_code', {
-        phoneNumber: '4153518780',
-        message: 'This is the code to verify your account'
-    }).save((err) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(`Notification job created: ${job.id}`);
-        }
-});
+const queue = kue.createQueue({ redis: redisClient });
+const job = queue
+  .create("push_notification_code", {
+    phoneNumber: "4153518780",
+    message: "This is the code to verify your account",
+  })
+  .save((err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
 
 function sendNotification(phoneNumber, message) {
-        console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  console.log(
+    `Sending notification to ${phoneNumber}, with message: ${message}`
+  );
 }
 
-queue.process('push_notification_code', (job, done) => {
-        const {phoneNumber, message} = job.data;
-        sendNotification(phoneNumber, message);
-        done();
+queue.process("push_notification_code", (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
 });
 
-queue.on('error', (err) => {
-        console.log(`Queue error: ${err}`);
+queue.on("error", (err) => {
+  console.log(`Queue error: ${err}`);
 });
